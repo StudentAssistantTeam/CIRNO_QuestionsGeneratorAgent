93fd8b181d9e90f1f3d9faee8ca9a3c91cbab1b3
NewtonZhan <zhanyunze0601@gmail.com>
2026-03-27 08:14:12 +0800

    feat(dev): Early developing

---
+++ questions_setter_agent/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+# env file path config
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE_PATH = BASE_DIR / "questions_setter_agent.env"
+
+
+# Settings
+class Settings(BaseSettings):
+    # LLM config
+    llm_model_name: str = ""
+    llm_api_key: str = ""
+    llm_base_url: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE_PATH,
+        env_file_encoding="utf-8",
+        extra="allow"
+    )
+
+
+settings = Settings()
